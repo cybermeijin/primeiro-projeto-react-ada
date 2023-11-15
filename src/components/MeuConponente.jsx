23ad62e49b14c65cbbd30eac5ffcdb2906b08a4f
Bowie <alexsandro.mello@gmail.com>
2023-11-15 10:19:32 -0300

    primeiro commit

---
+++ src/components/MeuConponente.jsx
@@ -0,0 +1,9 @@
+export default function MeuComponente() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est rem
+      assumenda quod porro provident nemo? Excepturi ex, provident veniam
+      pariatur deleniti a optio corrupti eius. Laborum eos aut alias.
+    </p>
+  );
+}
